348e64173e577e4caa0761d8bfa75f391413b542
Enzo0013929 <enzolimaflorencio@gmail.com>
2024-11-24 21:49:06 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+body {
+    font-family: "Sorts Mill Goudy", serif;
+    font-style: italic;
+    background: rgb(82, 4, 4);
+    color: dodgerblue;
+    justify-content: center;
+    align-items: center;
+    height: 1000;
+    font-size: 26px;
+}
+
+button {
+    background: rgba(245, 96, 51, 0.973);
+    padding-bottom: 5px;
+}
+
+p {
+    display: flex;
+    border-bottom: 4px solid lightseagreen;
+}
